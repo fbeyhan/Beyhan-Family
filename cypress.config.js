01a64203d7533785db5b608630df8253083010ab
fbeyhan <51765916+fbeyhan@users.noreply.github.com>
2025-12-18 23:59:03 -0500

    Temporarily exclude finance Cypress tests from deployment

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   e2e: {
     baseUrl: 'http://localhost:5173',
-    specPattern: 'cypress/e2e/**/*.cy.{js,ts}'
+    specPattern: 'cypress/e2e/**/*.cy.{js,ts}',
+    excludeSpecPattern: [
+      'cypress/e2e/finance*.cy.ts'
+    ]
   }
 }
